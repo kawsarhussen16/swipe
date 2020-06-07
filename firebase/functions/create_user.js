39a145d2b197c2fefe6986667705dfc50105751a
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-06-06 20:44:36 -0400

    sanitized user input

--- firebase/functions/create_user.js
+++ firebase/functions/create_user.js
@@ -1,3 +1,12 @@
 module.exports = function (req, res) {
-  res.send(req.body);
+  //verify the phone number
+  if (!req.bidy.phone) {
+    return res.status(422).send({ err: "Bad Input" });
+  }
+
+  // format the phone number
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+  // create a user account using that phone number
+
+  // respond to the phone number
 };
