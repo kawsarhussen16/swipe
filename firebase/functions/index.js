a050b236a59ea89d51ed7535a3985debf6a12e3f
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-06-06 20:32:08 -0400

    Firbase app created

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,10 @@
 const functions = require("firebase-functions");
+const createUser = require("./create_user");
+const admin = require("firebase-admin");
+const serviceAccount = require("./service_account.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://swipe-257b1.firebaseio.com",
 });
+exports.createUser = functions.https.onRequest(createUser);
